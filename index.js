bb10f729a6d0b1ed476365ccf1ce8b46a2f1edc5
TomChambrier <tomw08@gmail.com>
2016-02-27 13:16:41 +0000

    added divide function

--- index.js
+++ index.js
@@ -2,4 +2,9 @@ function add(num1, num2){
     return num1 + num2;
 }
 
+function divide(num1, num2){
+    return num1 / num2;
+}
+
 module.exports.add = add;
+module.exports.divide = divide;
--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 var expect = require('chai').expect;
 var index = require ('../index');
 
+// tests add function
 describe('add', function() {
     describe('#add()', function () {
         it('should return 4 when given 2 and 2', function () {
